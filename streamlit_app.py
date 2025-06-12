f74d6622d78b5532c0d695ae790f152a56fe52d7
faham <faham084@gmail.com>
2025-06-12 06:46:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,4 @@ st.title('Machine Learning App 🦾')
 
 st.info('This app builds machine learning models')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df.columns
+st.write("**Raw Data**')
