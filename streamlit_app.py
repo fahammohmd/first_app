0d27d788a02cfa9e34e722b8259dac6c2857d90c
faham <faham084@gmail.com>
2025-06-12 06:43:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('Machine Learning App 🦾')
 
 st.info('This app builds machine learning models')
 
-df = pd.read_csv('https://github.com/fahammohmd/data/blob/main/Indian%20Urban%20Air%20Quality%20and%20Health%20Impact%202019-2024.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df.columns
